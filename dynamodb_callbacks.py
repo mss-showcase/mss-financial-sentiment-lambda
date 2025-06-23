71a58d8f1aa47c3e8a92cb258b3b26c7758d9769
Malyik József <jozsef.malyik@barre.hu>
2025-06-24 00:49:51 +0200

    fixing persistence error

--- dynamodb_callbacks.py
+++ dynamodb_callbacks.py
@@ -8,12 +8,27 @@ dynamodb = boto3.resource("dynamodb")
 article_table = dynamodb.Table(DYNAMODB_SENTIMENT_ARTICLES_TABLE)
 feed_table = dynamodb.Table(DYNAMODB_SENTIMENT_FEEDS_TABLE)
 
+def get_last_feed_pubdate_callback(feed_url):
+    """
+    Gets the last processed pubDate for a feed.
+    """
+    resp = feed_table.get_item(Key={"id": feed_url})
+    if "Item" in resp:
+        return resp["Item"].get("last_pubdate")
+    return None
+
+def set_last_feed_pubdate_callback(feed_url, pubdate):
+    """
+    Sets the last processed pubDate for a feed.
+    """
+    feed_table.put_item(Item={"id": feed_url, "last_pubdate": pubdate})
+
 def check_processed_callback(url, article_pubdate=None):
     """
     Checks if the article URL is already processed.
     Optionally, can check pubdate if needed.
     """
-    resp = article_table.get_item(Key={"url": url})
+    resp = article_table.get_item(Key={"id": url})
     if "Item" in resp:
         # Optionally, compare pubdate if provided
         if article_pubdate and "pubdate" in resp["Item"]:
@@ -27,19 +42,5 @@ def persist_callback(record):
     """
     ttl_seconds = int(time.time()) + 60 * 24 * 60 * 60  # 60 days from now
     record["ttl"] = ttl_seconds
+    record["id"] = record["url"]  # Ensure id is set for the hash key
     article_table.put_item(Item=record)
-
-def get_last_feed_pubdate_callback(feed_url):
-    """
-    Gets the last processed pubDate for a feed.
-    """
-    resp = feed_table.get_item(Key={"feed_url": feed_url})
-    if "Item" in resp:
-        return resp["Item"].get("last_pubdate")
-    return None
-
-def set_last_feed_pubdate_callback(feed_url, pubdate):
-    """
-    Sets the last processed pubDate for a feed.
-    """
-    feed_table.put_item(Item={"feed_url": feed_url, "last_pubdate": pubdate})
